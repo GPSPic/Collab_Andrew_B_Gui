77f5361e2802f9f094cad5fba0193bf90ebc97e9
GPSPic <picard.guillaume47@gmail.com>
2023-04-04 15:55:46 +0100

    starting hw

---
+++ models/park.js
@@ -0,0 +1,10 @@
+const Park = function (name, ticketPrice, dinosaurCollection) {
+    this.name = name
+    this.ticketPrice = ticketPrice
+    this.dinosaurCollection = dinosaurCollection
+}
+
+// run npm init in terminal
+// install mocha (npm install --save-dev mocha)
+// modify scripts command in package.json (example: mocha specs)
+// run command npm test from root folder, which will call mocha specs
