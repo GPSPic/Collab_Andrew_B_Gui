a2e89e83c0aa61ea539e5b8fa0108368e1404e65
GPSPic <picard.guillaume47@gmail.com>
2023-06-01 14:27:09 +0100

    added empty front end with a fetch

--- courseservice/course_client/src/App.js
+++ courseservice/course_client/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import { useState, useEffect } from "react";
+
 
 function App() {
 
+  const [course, setCourse] = useState(null)
 
+  useEffect(() => {
+    fetch("http://localhost:8080/courses")
+    .then(res => res.json())
+    .then(data => setCourse(data))
+  }, [])
+
+
+
+  
 
   
   return (
